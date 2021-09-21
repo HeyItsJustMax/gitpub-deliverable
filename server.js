af0012695f345f74fdc871c5fce527e0fd1047b7
Maximilian Rice <maximilian.rice1@gmail.com>
2021-09-21 10:50:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// require express
+const express = require("express")
+// set express() to a variable
+const app = express()
+// set port variable to 3000
+const port = 3000
+// set up drinks data so it can be required
+const drinks = require("./models/drinks")
+
+// include a get route 
+app.get("/", (req, res) => {
+    res.send('Welcome to the Gitpup App!')
+})
+
+// get route for drinks
+app.get("/drinks", (req, res) => {
+    res.render("drinks_index.ejs",)
+})
+
+// get route for /drinks/:id
+app.get("/drinks/:id", (req, res) => {
+    res.send(req.params.id)
+})
+
+// set up app to listen to assigned port#
+app.listen(port, () => {
+        console.log('listening')
+    }
+)
